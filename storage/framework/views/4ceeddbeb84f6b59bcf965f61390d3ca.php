a395be0f70c28c9204497aeda6fefff00b2ac1a9
MohamedAbdelHalim1 <88633374+MohamedAbdelHalim1@users.noreply.github.com>
2024-05-04 01:44:04 +0300

    upload storage

---
+++ storage/framework/views/4ceeddbeb84f6b59bcf965f61390d3ca.php
@@ -0,0 +1,141 @@
+<header class="rtl">
+<div class="container-fluid">
+    <div class="row justify-content-between">
+<nav class="navbar nav-mob" style="background-color: #3736AF;">
+<div class="container-fluid">
+    <a class="navbar-brand" href="index.html">
+        <img src="assets/images/logo-white.png" style="width: 140px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+aria-labelledby="offcanvasNavbarLabel">
+<div class="offcanvas-header">
+    <h5 class="offcanvas-title fw-bolder" id="offcanvasNavbarLabel">مندوبي</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+        aria-label="Close"></button>
+</div>
+  <div class="offcanvas-body">
+
+    <nav class="menu">
+        <ul>
+            <li class="mb-4">
+                <a href="<?php echo e(route('nationalities.index')); ?>" class="main-nav">
+                    <i class="fa-regular fa-flag ms-3 fw-semibold"></i>جنسيات
+                </a>
+            </li>
+            <li class="mb-4">
+                <a class="button-ser-small" id="menuButton-ser-small">
+                    <i class="fa-solid fa-bars ms-3 fw-semibold"></i>خدمات
+                    <i class="fa-solid fa-caret-down me-3"></i>
+                </a>
+                <nav id="menu-ser-small" class="menu-ser-small mt-3">
+                    <ul class="me-3">
+                        <li class="mb-2">
+                            <a href="main-service.html">
+                                <i class="fa-solid fa-square ms-2"></i>خدمات رئيسية
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="sub-service.html">
+                                <i class="fa-solid fa-square ms-2"></i>خدمات فرعية
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </li>
+            <li class="mb-4">
+                <a class="button-unv-small" id="menuButton-unv-small">
+                    <i class="fa-solid fa-building-columns ms-3 fw-semibold"></i>جامعات
+                    <i class="fa-solid fa-caret-down me-3"></i>
+                </a>
+                <nav id="menu-unv-small" class="menu-unv-small mt-3">
+                    <ul class="me-3">
+                        <li class="mb-2">
+                            <a href="add-university.html">
+                                <i class="fa-solid fa-square ms-2"></i>أضافة جامعة
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="university-details.html">
+                                <i class="fa-solid fa-square ms-2"></i>تفاصيل جامعة
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </li>
+            <li class="mb-4">
+                <a href="faculty.html" class="main-nav">
+                    <i class="fa-solid fa-graduation-cap ms-3 fw-semibold"></i>كليات
+                </a>
+            </li>
+            <li class="mb-4">
+                <a href="majors.html" class="main-nav">
+                    <i class="fa-solid fa-angles-down  ms-3 fw-semibold"></i>تخصصات
+                </a>
+            </li>
+            <li class="mb-4">
+                <a href="orders.html" class="main-nav">
+                    <i class="fa-solid fa-list-check ms-3 fw-semibold"></i>طلبات
+                </a>
+            </li>
+            <li class="mb-4">
+                <a href="members.html" class="main-nav">
+                    <i class="fa-solid fa-user-group ms-3 fw-semibold"></i>اعضاء
+                </a>
+            </li>
+            <li class="mb-4">
+                <a href="users.html" class="main-nav">
+                    <i class="fa-solid fa-user-check ms-3 fw-semibold"></i>المستخدمين
+                </a>
+            </li>
+            <li class="mb-4">
+                <a href="news.html" class="main-nav">
+                    <i class="fa-solid fa-envelope-open-text ms-3 fw-semibold"></i>اخبار
+                </a>
+            </li>
+        </ul>
+    </nav>
+    </div>
+</div>
+</div>
+</nav>
+<div class="col-xl-2 col-lg-3 col-md-3 p-3 nav-pc" style="background-color: #3736AF;">
+                    <div class="d-flex justify-content-center">
+                        <a href="index.html">
+                            <img src="assets/images/logo-white.png" style="width: 140px;">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-10 col-lg-9 col-md-9" style="background-color: #22219A;">
+                    <div class="d-flex justify-content-end ms-xl-5 ms-lg-5 ms-md-5 top-side">
+                        <div class="d-flex justify-content-end mt-3">
+                            <div class="square ms-2">
+                                <i class="fa-regular fa-user icon"></i>
+                            </div>
+                            <div class="square ms-2">
+                                <i class="fa-regular fa-bell icon"></i>
+                            </div>
+                            <div class="square ms-5">
+                                <i class="fa-solid fa-gear icon"></i>
+                            </div>
+                        </div>
+                        <div class="text-white ms-2 mt-3">
+                            <h3>وداد اشرف
+                                <br>
+                                <span class="fw-lighter">
+                                    @wedad.ash
+                                </span>
+                            </h3>
+                        </div>
+                        <div class="circle mt-2">
+                            <img src="assets/images/IMG_20220205_105633_659.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header><?php /**PATH E:\mandoby\resources\views/menu.blade.php ENDPATH**/ ?>
